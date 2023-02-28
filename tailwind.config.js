2632f664cbf00ce79e21916ba847111ebcb8c1e8
Wyatt Haak <wyatt.haak4@gmail.com>
2023-02-27 21:50:48 -0800

    finished the project final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.blueGray,
+      },
+      fontFamily: {
+        outfit: ['"Outfit"', ...defaultTheme.fontFamily.sans]
+      },
+    },
   },
   plugins: [],
 }
